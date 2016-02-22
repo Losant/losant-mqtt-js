d5e5e6f0792404eb57e256ef7c9c6ac438b66a9e
Brandon Cannaday <brandon@modulus.io>
2016-02-22 15:20:01 -0500

    Added edison example.

---
+++ examples/intel-edison.js
@@ -0,0 +1,62 @@
+/**
+ * Example demonstrating how to collect temperature data
+ * and report it to the Structure IoT developer platform.
+ *
+ * This example assumes the Edison is attached to the
+ * Arduino Breakout board with the following connections.
+ *
+ * LED connected to GPIO pin 7.
+ * TMP36 temperature sensor connected to analog input A0.
+ * 
+ * Copyright (c) 2016 Structure. All rights reserved.
+ * http://www.getstructure.io
+ */
+
+var mraa = require('mraa');
+var Device = require('structure-sdk-js').Device;
+
+// Reading temperature from analog input.
+var temp = new mraa.Aio(0);
+
+// Blinking an LED everytime temperature is read.
+var led = new mraa.Gpio(7);
+led.dir(mraa.DIR_OUT);
+
+// Construct a device instance.
+var device = new Device({
+  id: 'my-device-id',
+  key: 'my-access-key',
+  secret: 'my-access-secret'
+});
+
+// Connect device to Structure.
+device.connect();
+
+// Attach event listener for commmands.
+device.on('command', function(command) {
+  console.log(command.name);
+  console.log(command.payload);
+});
+
+
+// Once a second, read the temp and report to Structure.
+setInterval(function() {
+  
+  // Turn on the LED.
+  led.write(1);
+  
+  // Read temp voltage and convert.
+  var tempRaw = temp.read();
+  var degreesC = (((tempRaw / 1024) * 5) - 0.52) * 100;
+  var degreesF = degreesC * 1.8 + 32;
+  
+  console.log(degreesC);
+  console.log(degreesF);
+
+  // Report state to Structure.
+  device.sendState({ temp: degreesF });
+  
+  // Wait a little and turn off the LED.
+  setTimeout(function() { led.write(0); }, 500);
+  
+}, 1000);
